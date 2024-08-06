ab8b2e8d923da6828e91460546439264cb1cce09
tanisheikh <tanisheikh2097@gmail.com>
2024-08-06 19:39:53 +0530

    Env File changes

--- react_repo/src/Home.jsx
+++ react_repo/src/Home.jsx
@@ -1,5 +1,52 @@
+import { useEffect, useState } from "react";
+
 const Home = () => {
-  return <div>Home</div>;
+  const [data, setData] = useState();
+  const [error, setError] = useState();
+  const apiUrl = process.env.REACT_APP_API_URL;
+  //   const apiUrl = "https://jsonplaceholder.typicode.com/posts/1";
+
+  useEffect(() => {
+    if (!apiUrl) {
+      console.error("API URL is not defined in environment variables.");
+      return;
+    }
+
+    fetch(apiUrl)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then((_data) => {
+        console.log("_data", _data);
+        setData(_data);
+      })
+      .catch((err) => {
+        console.error("Fetch error:", err);
+        setError(err);
+      });
+  }, [apiUrl]);
+
+  return (
+    <div>
+      {data && data.length > 0 ? (
+        <div>
+          {data.map((item) => {
+            return (
+              <>
+                <p> Id: {item.id}</p>
+                <p>Title: {item.title}</p>
+              </>
+            );
+          })}
+        </div>
+      ) : (
+        ""
+      )}
+    </div>
+  );
 };
 
 export default Home;
